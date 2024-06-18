a6ce66c843e0420606c2a02f8283705fdd6af380
Lu <sabrina.tanglu@gmail.com>
2024-06-18 09:55:27 -0700

    update UI to make the image covering whole screen

--- lib/presentation/checked_asteroids_screen.dart
+++ lib/presentation/checked_asteroids_screen.dart
@@ -23,61 +23,76 @@ class _CheckedAsteroidsScreenState extends State<CheckedAsteroidsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Checked Asteroids'),
-        ),
-        body: Stack(
-          children: [
-            const NetworkImageWidget(
-                'https://d.newsweek.com/en/full/1043290/7-26-asteroids.jpg'),
-            Center(
-              child: LayoutBuilder(builder: (context, constraints) {
-                return Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: SizedBox(
-                    width: min(constraints.maxWidth, 600),
-                    child: ListView.builder(
-                      itemCount: widget.checkedAsteroids.length,
-                      itemBuilder: (context, index) {
-                        final asteroid = widget.checkedAsteroids[index];
-                        return Card(
-                          color: Colors.black54,
-                          child: ListTile(
-                            title: Text(asteroid.name,
-                                style: const TextStyle(color: Colors.white)),
-                            subtitle: Text(
-                                'Hazardous: ${asteroid.isHazardous ? 'Yes' : 'No'}',
-                                style: const TextStyle(color: Colors.white70)),
-                            trailing: Checkbox(
-                              activeColor: Colors.white,
-                              checkColor: Colors.black,
-                              value: asteroid.checked,
-                              onChanged: (bool? value) {
-                                toggleCheck(index);
-                              },
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                );
-              }),
+    return Stack(
+      children: [
+        const NetworkImageWidget(
+            'https://d.newsweek.com/en/full/1043290/7-26-asteroids.jpg'),
+        Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back, color: Colors.white),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              title: const Text('Checked Asteroids',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold)),
             ),
-          ],
-        )
+            body: Stack(
+              children: [
+                Center(
+                  child: LayoutBuilder(builder: (context, constraints) {
+                    return Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: SizedBox(
+                        width: min(constraints.maxWidth, 600),
+                        child: ListView.builder(
+                          itemCount: widget.checkedAsteroids.length,
+                          itemBuilder: (context, index) {
+                            final asteroid = widget.checkedAsteroids[index];
+                            return Card(
+                              color: Colors.black54,
+                              child: ListTile(
+                                title: Text(asteroid.name,
+                                    style:
+                                        const TextStyle(color: Colors.white)),
+                                subtitle: Text(
+                                    'Hazardous: ${asteroid.isHazardous ? 'Yes' : 'No'}',
+                                    style:
+                                        const TextStyle(color: Colors.white70)),
+                                trailing: Checkbox(
+                                  activeColor: Colors.white,
+                                  checkColor: Colors.black,
+                                  value: asteroid.checked,
+                                  onChanged: (bool? value) {
+                                    toggleCheck(index);
+                                  },
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ],
+            )
 
-        // ListView.builder(
-        //   itemCount: checkedAsteroids.length,
-        //   itemBuilder: (context, index) {
-        //     final asteroid = checkedAsteroids[index];
-        //     return ListTile(
-        //       title: Text(asteroid.name),
-        //       subtitle: Text('Hazardous: ${asteroid.isHazardous ? 'Yes' : 'No'}'),
-        //     );
-        //   },
-        // ),
-        );
+            // ListView.builder(
+            //   itemCount: checkedAsteroids.length,
+            //   itemBuilder: (context, index) {
+            //     final asteroid = checkedAsteroids[index];
+            //     return ListTile(
+            //       title: Text(asteroid.name),
+            //       subtitle: Text('Hazardous: ${asteroid.isHazardous ? 'Yes' : 'No'}'),
+            //     );
+            //   },
+            // ),
+            ),
+      ],
+    );
   }
 }
